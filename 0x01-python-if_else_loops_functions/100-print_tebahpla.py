7b1d6ec973733601a8e83b5a83ce017d98267b43
Dawitt-dev <davoza27@yahoo.com>
2023-11-28 19:01:10 -0800

    just_trying_1

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for ascii_value in range(ord('z'), ord('a') - 1, -1):
     if ascii_value % 2 == 0:
-        char = chr(ascii_value) 
+        char = chr(ascii_value)
     else:
         char = chr(ascii_value - 32)
     print("{}".format(char), end='')
