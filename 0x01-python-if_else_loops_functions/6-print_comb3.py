ffc658844209d87bb29b5b9da5420e3bdfd090b1
Dawitt-dev <davoza27@yahoo.com>
2023-11-28 17:20:58 -0800

    task_6.0 edited

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for number in range(10):
-    for num in range(number +1, 10):
-        print("{:d}{:d}".format(number, num), end=', ' if number < 9 or num < 8 else '\n')
+    for num in range(number + 1, 10):
+        if number == 8 and num == 9:
+            print("{:d}{:d}".format(number, num))
+        else:
+            print("{:d}{:d}, ".format(number, num), end='')
