90fd206ac0cdc0061201259650956bbc922a2480
Dawitt-dev <davoza27@yahoo.com>
2023-11-28 17:39:57 -0800

    task_7.0 edited

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
-    return ord('a') <= ord(c) <= ord('Z')
+    return ord('a') <= ord(c) <= ord('z') 
+print("a is {}".format("lower" if islower("a") else "upper"))
+
