78f32adebbcaf91677915c33d1274cfa05e8b531
Dawitt-dev <davoza27@yahoo.com>
2023-12-03 17:36:05 -0800

    task_5.0 edited

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     new_string = ""
     for ch in my_string:
         if (ch != "c" and ch != "C"):
-            new_string +=ch
+            new_string += ch
     return new_string
