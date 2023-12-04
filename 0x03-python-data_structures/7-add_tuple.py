70746261802e485d4466a53a4d1edb5201df8367
Dawitt-dev <davoza27@yahoo.com>
2023-12-04 15:00:31 -0800

    task_7. Tuples addition

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,22 @@
+#!/usr/bun/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_sum = (0, 0)
+
+    if  len(tuple_a) < 2 and len(tuple_b) < 2:
+        return tuple_sum
+    elif len(tuple_a) < 2 and len(tuple_b) >= 2:
+        if not tuple_a:
+            tuple_sum = (tuple_b[0], tuple_b[1])
+        else:
+            tuple_sum = (tuple_a[0] + tuple_b[0], tuple_b[1] + tuple_sum[1])
+    elif len(tuple_a) >= 2 and len(tuple_b) < 2:
+        if not tuple_b:
+            tuple_sum = (tuple_a[0], tuple_a[1])
+        else:
+            tuple_sum = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_sum[1])
+    else:
+        tuple_sum = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    
+    return tuple_sum
+
+
