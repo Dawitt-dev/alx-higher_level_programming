75115172436e15d357c18a775e30e227cd809906
Dawitt-dev <davoza27@yahoo.com>
2023-12-04 08:02:43 -0800

    task.6.1 edited for spaces

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return
     for row in matrix:
-        for element in row:
-            print("{:2d}".format(element), end='')
+        for i in range(len(row)):
+            print("{:d}".format(row[i]), end="")
+            if i < len(row) - 1:
+                print(" ", end="")
         print()
