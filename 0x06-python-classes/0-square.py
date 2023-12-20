aaff67ff676c97e19b123262e78c9fd32b1184b2
Dawitt-dev <davoza27@yahoo.com>
2023-12-19 17:03:02 -0800

    task_0.1 edited

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 class Square:
     """
-    Square is my first class
-
+    Square is my first class.
     """
 
     def __init__(self):
         """
-        Initializes
+        Initializes a new instance of the Square class.
         """
 
         pass
