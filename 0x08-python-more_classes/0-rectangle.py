e70587116d9a90f21954c21ae765d82eadcf7504
Dawitt-dev <davoza27@yahoo.com>
2024-01-04 17:20:40 -0800

    task_0.3 edited

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
+"""
+This module defines a Rectangle class.
+"""
 
 
 class Rectangle:
-    """A class that defined a rectangle."""
-
+    """
+    A class that defined a rectangle.
+    """
     def __int__(self):
-        """Initialize an instance of the Rectangle class."""
+        """Initialize an instance of the Rectangle class.
+        """
         pass
+
+
+# 'print(__import__("0-rectangle").__doc__)', prints the module docstring.
+# 'print(__import__("0-rectangle").Rectangle.__doc__)', prints the class docst
+# >ring.
+# 'print(__import__("0-rectangle").Rectangle.__init__.__doc__)', prints the me
+# >thods docstring.
