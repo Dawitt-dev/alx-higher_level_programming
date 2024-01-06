fc31f1e316940c164ead98399f1bc3d22b54f721
Dawitt-dev <davoza27@yahoo.com>
2024-01-05 18:10:51 -0800

    documentation added

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,8 +5,15 @@
 
 class LockedClass:
     """A locked class allowing only dynamic creation of the 'first_name'
-    instance attribute."""
+    instance attribute.
+    
+
+    Attributes:
+        first_name (str): first name of something.
+    """
      __slots__ = ["first_name"]
 
      def __init__(self, first_name=""):
+          """Creates new instances of Locked Class."""
+
          self.first_name = first_name
