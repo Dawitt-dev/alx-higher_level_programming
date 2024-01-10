6fbc7313901d6ecb2dc58d008c76b3fce1d8aba6
Dawitt-dev <davoza27@yahoo.com>
2024-01-09 18:05:05 -0800

    task_9. Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Defining the class student"""
+
+
+class student:
+    """
+    Defines students properties.
+
+    Attributes:
+        first_name (str): first name of a student.
+        last_name (str): last name of a student.
+        age (int): age of a student.
+    """
+    def __init__(self, first_name, last_name, age):
+        """Creates new instances of a student.
+
+
+        Args:
+            first_name (str): first name of a student.
+            last_name (str): last name of a student.
+            age (int): age of a student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ retrieves a dictionary representation of a Student
+
+
+        Returns:
+            dict: dictionary representation of a student.
+        """
+        return self.__dict__
