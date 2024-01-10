13c01def193dff95fd25f4d5d610f7566e538bc8
Dawitt-dev <davoza27@yahoo.com>
2024-01-09 18:09:52 -0800

    task_9. Student to JSON

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -31,4 +31,8 @@ class student:
         Returns:
             dict: dictionary representation of a student.
         """
-        return self.__dict__
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
