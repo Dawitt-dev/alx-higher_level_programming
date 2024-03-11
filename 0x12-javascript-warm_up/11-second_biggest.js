308fa26e9f225c3b13c0fa2fea9e820b80c9c528
Dawitt-dev <davoza27@yahoo.com>
2024-03-11 15:51:51 -0700

    task_11. Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,9 @@
 #!/usr/bin/node
 
-function secondBiggest (args) {
-  const numbers = args.map(Number);
-
-  if (numbers.length <= 2) {
-    return 0;
-  }
-
-  const uniqueNumbers = [...new Set(numbers)];
-  const sortedNumbers = uniqueNumbers.sort((a, b) => b - a);
-
-  return sortedNumbers[1];
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const numbers = process.argv.slice(2).map(Number);
+  const sortedNumbers = numbers.sort((a, b) => b - a);
+  console.log(sortedNumbers[1]);
 }
-
-const result = secondBiggest(process.argv.slice(2));
-console.log(result);
