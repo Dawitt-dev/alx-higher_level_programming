dfaf50dc63db27aa72311d4bde9495d766dfec4e
Dawitt-dev <davoza27@yahoo.com>
2024-03-12 18:08:24 -0700

    task9 and task4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -7,19 +7,21 @@ class Rectangle {
     }
   }
 
-  print() {
+  print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
-  rotate() {
-	  let temp = this.height;
-	  this.height = this.width;
-	  this.width = temp;
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
   }
-  double() {
-	  this.height *= 2;
-	  this.width *= 2;
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
   }
 }
 
