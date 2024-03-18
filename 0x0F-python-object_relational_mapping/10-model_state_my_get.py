9ac99f72f6857553546a440c3c248ef420152f92
Dawitt-dev <dawittbeyene22@gmail.com>
2024-03-18 05:18:06 -0700

    task_10.Get state

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Script that lists all State objects from the database hbtn_0e_6_usa.
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Database connection parameters
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    name = sys.argv[4]
+
+    # Database connection URL
+    db_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    db_url = db_url.format(username, password, database, name)
+
+    # Create engine
+    engine = create_engine(db_url)
+
+    # Create all tables in the engine
+    Base.metadata.create_all(engine)
+
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # Create a Session
+    session = Session()
+
+    # Query all State objects and print them
+    states = session.query(State).filter(State.name == name).first()
+    if states:
+        print("{}".format(states.id))
+    else:
+        print("Not found")
+
+    # Close the session
+    session.close()
