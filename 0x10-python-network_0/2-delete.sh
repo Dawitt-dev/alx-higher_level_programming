101d67000e83656d8eac5b6374f78272371a57f9
Dawitt-dev <davoza27@yahoo.com>
2024-03-27 18:48:10 -0700

    task_2

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sends a DELETE request to the URL passed as the 1st arg and displays the body of response
+#sends DELETE request to the URL passed as the 1st arg and displays the body of response
 curl -sX DELETE "$1"
