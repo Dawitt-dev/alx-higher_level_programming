c6c05e6eadbcdc9fb982f8acb3bc54f2545ac8e4
Dawitt-dev <davoza27@yahoo.com>
2024-04-23 06:57:46 -0700

    task_1.Writeme

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+// Get file path
+const filePath = process.argv[2];
+
+// Get string content to be written
+const stringContent = process.argv[3];
+
+// check if file path provided
+if (!filePath) {
+  console.error('please provide file path as an argument');
+  process.exit(1);
+}
+
+// check if string to be written provided
+if (!stringContent) {
+  console.error('No string provided');
+  process.exit(1);
+}
+
+// write the content on file
+fs.writeFile(filePath, stringContent, err => {
+  if (err) {
+    console.error(err);
+  }
+});
