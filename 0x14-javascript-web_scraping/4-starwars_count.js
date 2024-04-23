d09e9f1a06bd26ced5869cd3eca63547be2cc47f
Dawitt-dev <davoza27@yahoo.com>
2024-04-23 08:02:20 -0700

    task_4. Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+const request = require('request');
+
+// Get the API URL from the command line arguments
+const apiUrl = process.argv[2];
+
+// check if API URL is provided
+if (!apiUrl) {
+  console.error('Please provide the API URL');
+  process.exit(1);
+}
+
+// Make a GET request to the API endpoint
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  // Parse the JSON response body
+  const films = JSON.parse(body).results;
+
+  // Count the number of movies where "wedge Antilles"
+  const count = films.filter(film =>
+    film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
+  ).length;
+
+  // print the nuber of movies
+  console.log(count);
+});
