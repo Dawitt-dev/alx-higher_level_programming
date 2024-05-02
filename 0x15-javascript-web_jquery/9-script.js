13dca2a68c674b25f7dbc92dcef0f195de17fc84
Dawitt-dev <davoza27@yahoo.com>
2024-05-02 16:34:27 -0700

    all tasks

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,9 @@
+dy(function(){
+    $.ajax({
+        url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
+        type: 'GET',
+        success: function(data) {
+            $('#hello').text(data.hello);
+        }
+    });
+});
